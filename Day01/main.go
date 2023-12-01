7b087eefaaa1a2dc970320f4a5b16244d7e7f91d
Fraser <fraserws.uk2@gmail.com>
2023-12-01 19:49:15 +0000

    day01 pt2

--- Day01/main.go
+++ Day01/main.go
@@ -15,7 +15,8 @@ func main() {
 	}
 
 	inputStrings := string(b)
-	lines := strings.Split(inputStrings, "\n")
+	lines := strings.Split(convertWrittenToNumeric(inputStrings), "\n")
+
 	extractedNumbers := make([][]string, len(lines))
 	re := regexp.MustCompile(`\d+`)
 
@@ -26,7 +27,9 @@ func main() {
 
 	for _, numbers := range extractedNumbers {
 		if len(numbers) > 0 {
-			concatenated := string(numbers[0][0]) + string(numbers[len(numbers)-1][len(numbers[len(numbers)-1])-1])
+			firstDigit := numbers[0][:1]
+			lastDigit := numbers[len(numbers)-1][len(numbers[len(numbers)-1])-1:]
+			concatenated := firstDigit + lastDigit
 			num, err := strconv.Atoi(concatenated)
 			if err != nil {
 				panic(err)
@@ -36,4 +39,25 @@ func main() {
 	}
 
 	fmt.Println("Total:", total)
+}
+
+func convertWrittenToNumeric(text string) string {
+	numberMap := map[string]string{
+		"one":   "o1one",
+		"two":   "t2two",
+		"three": "t3three",
+		"four":  "f4four",
+		"five":  "f5five",
+		"six":   "s6six",
+		"seven": "s7seven",
+		"eight": "e8eight",
+		"nine":  "n9nine",
+		"zero":  "z0zero",
+	}
+
+	for key, value := range numberMap {
+		text = strings.Replace(text, key, value, -1)
+	}
+
+	return text
 }
